7d02eadfbaf588753cf3668c34c667427ca9bf02
hrollin5 <haley@haleyframpton.com>
2023-10-05 20:52:51 -0600

    Build Flask powered API

--- pickleapp.py
+++ pickleapp.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sqlite3
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session, sessionmaker
@@ -7,10 +7,11 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 
+
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///florida_full_db.sqlite")
+engine = create_engine("sqlite:///florida_info_db.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -18,7 +19,7 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save reference to the table
-# courts = Base.classes.court_location
+courts = Base.classes.court_location
 elderly = Base.classes.elderly_people
 
 #################################################
@@ -36,26 +37,68 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/elderly"
+        f"/courts_complete<br/>"
+        f"/elderly_complete<br/>"
+        f"/county_data"
     )
 
 
-@app.route("/api/v1.0/elderly")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+@app.route('/courts_complete', methods=['GET'])
+def courts():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('florida_info_db.sqlite')  # Replace with your database name
+    # Create a cursor object
+    cursor = conn.cursor()
+    # Execute an SQL query to select all rows from the table
+    cursor.execute('SELECT * FROM court_location')  # Replace with your table name
+    # Fetch all rows from the query result
+    rows = cursor.fetchall()
+    # Close the database connection
+    conn.close()
+    # Convert the rows to a list of dictionaries
+    court_data = [{'Location Name': row[0], 'City': row[1], 'Number of Courts': row[2], 'County': row[3], "Latitude": row[4], "Longitude": row[5]} for row in rows]
+    # Return the data as JSON
+    return jsonify(court_data)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(elderly.County).all()
 
-    session.close()
+@app.route('/elderly_complete', methods=['GET'])
+def elderly():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('florida_info_db.sqlite')  # Replace with your database name
+    # Create a cursor object
+    cursor = conn.cursor()
+    # Execute an SQL query to select all rows from the table
+    cursor.execute('SELECT * FROM elderly_people')  # Replace with your table name
+    # Fetch all rows from the query result
+    rows = cursor.fetchall()
+    # Close the database connection
+    conn.close()
+    # Convert the rows to a list of dictionaries
+    data = [{'Total_Population': row[0], 'County_ID': row[1], 'Elderly_Population': row[2], 'County': row[3], "Percent_Elderly": row[4]} for row in rows]
+    # Return the data as JSON
+    return jsonify(data)
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+@app.route('/county_data', methods=['GET'])
+def base_map():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('florida_info_db.sqlite')  # Replace with your database name
+    # Create a cursor object
+    cursor = conn.cursor()
+    # Execute an SQL query to select all rows from the table
+    sql_query = """SELECT e.County, e.Percent_Elderly, SUM(c.'Number of Courts') AS TotalCourts
+                FROM elderly_people e
+                JOIN court_location c on e.County = c.County
+                GROUP BY e.County, e.Percent_Elderly"""
+    cursor.execute(sql_query)
+    # Fetch all rows from the query result
+    rows = cursor.fetchall()
+    # Close the database connection
+    conn.close()
+    # Convert the rows to a list of dictionaries
+    county_data = [{'County': row[0], 'Percent Elderly': row[1], 'Number of Courts': row[2]} for row in rows]
+    # Return the data as JSON
+    return jsonify(county_data)
 
-    return jsonify(all_names)
 
 if __name__ == '__main__':
     app.run(debug=True)
