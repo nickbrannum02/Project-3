2d6c00d2b836e6ec00a0063c2fd7f925464a77af
hrollin5 <haley@haleyframpton.com>
2023-10-07 15:35:39 -0600

    add sleep function

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,22 @@
-// JavaScript to initialize the map
-const map = L.map('map').setView([51.505, -0.09], 13);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    }
+      
+sleep(2000).then(() => { 
+    // JavaScript to initialize the map
+    const map = L.map("map", {
+        center: [27.96044, -82.30695],
+        zoom: 6
+    });
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+    
+    // L.marker([51.505, -0.09]).addTo(map)
+    //     .bindPopup('A sample location.')
+    //     .openPopup();
+    console.log(countyData.length); 
+});
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
-
-L.marker([51.505, -0.09]).addTo(map)
-    .bindPopup('A sample location.')
-    .openPopup();
+    
